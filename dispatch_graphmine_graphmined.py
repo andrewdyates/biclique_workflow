750399c3d1132d06616c85e725b73be81d06fb13
Andrew D. Yates <andrewyates.name@gmail.com>
2012-12-03 01:21:46 -0500

    working dispatcher

--- dispatch_graphmine_graphmined.py
+++ dispatch_graphmine_graphmined.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python
 """
-python loop_merge_bicliques_course.py outdir=$HOME/density_merge_bicliques
+python $HOME/pymod/biclique_workflow/dispatch_graphmine_graphmined.py outdir=$HOME/density_merge_bicliques dry=True
 """
+from __future__ import division
 import qsub
 import sys
+import numpy as np
+import merge_graphmine_graphmined
 
-CMD = "/usr/bin/time python /nfs/01/osu6683/density_merge_bicliques/merge_bicliques_course.py density=%(density)s merge=%(merge)s outdir=%(outdir)s"
+CMD = "python /nfs/01/osu6683/pymod/biclique_workflow/batch_graphmine_graphmined.py density=%(density)s merge=%(merge)s outdir=%(outdir)s in_fname=%(in_fname)s"
 
-def runloop(outdir="", dry=False):
+def runloop(outdir="", dry=False, dcor_filter=True, dcor_thresh=0.08):
+  if isinstance(dcor_filter,basestring) and dcor_filter.lower() in ('f', 'none', 'false'):
+    dcor_filter = False
+  dcor_thresh = float(dcor_thresh)
+  # generate input
+  in_fname = merge_graphmine_graphmined.main(outdir=outdir, dcor_filter=dcor_filter, dcor_thresh=dcor_thresh)
+  print "Generated %s." % in_fname
   for merge in ('0', '1'):
-    for density in map(str, (0.1, 0.2, 0.3, 0.325, 0.35, 0.375, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9)):
+    for density in map(lambda x: "%.3f"%x, np.arange(10,91,2.5)/100):
       jobname = "bcm_%s_%s" % (merge, density)
-      Q = qsub.Qsub(n_nodes=1, n_ppn=12, hours=20, work_dir=outdir, email=True, jobname=jobname)
-      cmd = CMD % {'density': density, 'merge': merge, 'outdir': outdir}
+      Q = qsub.Qsub(n_nodes=1, n_ppn=12, hours=3, work_dir=outdir, email=False, jobname=jobname)
+      cmd = CMD % {'density': density, 'merge': merge, 'outdir': outdir, 'in_fname': in_fname}
       Q.add(cmd)
       print Q.script()
       Q.submit(dry)
