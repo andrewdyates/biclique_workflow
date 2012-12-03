aa50659e7ad0b8d1892e6caa3748fd9ad26b26dd
Andrew D. Yates <andrewyates.name@gmail.com>
2012-12-02 20:23:58 -0500

    updated merge_bicliques algorithm

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ biclique_workflow
 =================
 
 Generate bicliques from rectangular dependency matrix using MAFIA workflow on qsub super computing environment.
+
+Depends on HeapDict data structure.
+http://pypi.python.org/pypi/HeapDict
--- merge_biclique_indices.py
+++ merge_biclique_indices.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 """Given a list of biclique indices, merge highly overlapping bicliques, filter out-of-bounds rows and columns.
 
+Merge percentage is:
+For each pair:
+- calcuate number of boxes in each biclique A, B = |rows| * |columns|
+- calculate number of boxes in overlap between bicliques I = |rows| * |columns|
+- A*pct >= I or B*pct >= I, merge
+- repeat in loop until convergence
+
+Requires HeapDict 1.0.0:
+  http://pypi.python.org/pypi/HeapDict
+
+
 EXAMPLES:
 
 python $HOME/pymod/biclique_workflow/merge_biclique_indices.py fname=/nfs/01/osu6683/net/gse15745_nov2/bicliques_pass5i3/Methyl-correct-aligned.pkl_mRNA-correct-aligned.pkl.DCOR.values_0.670+_adj0.500_sup.biclique.rowmapped.0.480_mergetype1_graphmined.output.1081.378.remapout k=5 n_rows=24334 n_cols=10277 > $HOME/gse15745_nov2012_experiments/bicliques/biclique5.R
@@ -16,6 +27,8 @@ python $HOME/pymod/biclique_workflow/merge_biclique_indices.py fname=$HOME/net/g
 python $HOME/pymod/biclique_workflow/merge_biclique_indices.py fname=$HOME/net/gse15745_nov2/bicliques_pass10i2/Methyl-correct-aligned.pkl_mRNA-correct-aligned.pkl.DCOR.values_0.400+_adj0.500_sup.biclique.rowmapped.0.280_mergetype1_graphmined.output.4131.2014.remapout k=10 n_rows=24334 n_cols=10277 > $HOME/gse15745_nov2012_experiments/bicliques/biclique10.R
 """
 from __future__ import division
+# Install from http://pypi.python.org/pypi/HeapDict
+from heapdict import heapdict
 import sys
 
 def main(fname, overlap=.60, k=1, n_rows=24334, n_cols=10277):
@@ -51,6 +64,48 @@ def main(fname, overlap=.60, k=1, n_rows=24334, n_cols=10277):
   for i, x in enumerate(merged):
     print "bc%s.%d.row<-c(%s)" % (k, i, ",".join(map(str, sorted(rows[x]))))
     print "bc%s.%d.col<-c(%s)" % (k, i, ",".join(map(str, sorted(cols[x]))))
+
+
+def merge_bicliques(bicliques, max_overlap=.55):
+  """Greedily merge biclique set B by maximum item overlap.
+  TODO: add minimum merged density constraint.
+
+  Args:
+    bicliques: [(row, col)], row=[int], col=[int] of bicliques to merge
+    max_overlap: float 0 <= x <= 1 of maximum allowable overlap of items without merge
+  Returns:
+    {}
+  """
+  # Calculate all pairs overlap
+  # a symmetric distance matrix that I can update
+  # a max heap to keep track of most overlapping biclique pair
+
+  B = dict(enumerate(bicliques)) # uniquely index bicliques, but allow constant access
+  D = heapdict()
+  for i in B:
+    for j in filter(lambda j:j>i, B):
+      # higher percentage overlap is lower "priority"
+      D[sorted(i,j)] -overlap(B[i], B[j])
+
+  # Merge top pair while it is above threshold
+  pair, overlap = D.popitem()
+  while -overlap > max_overlap:
+    i, j = sorted(pair)
+    B[i] = (B[i][0]|B[j][0], B[i][1]|B[j][1])
+    del B[j]
+    # Update all distances including i, delete those with j
+    for k in filter(lambda k:k!=i and k!=j, B):
+      D[sorted(i,k)] = -overlap(B[i], B[k])  # distance to merged biclique i
+      del D[sorted(j,k)]  # biclique j no longer exists
+    pair, overlap = D.popitem()  # get next pair
+  return B.values()
+
+def overlap(a, b):
+  """Compute overlap between bicliques a and b; biclique = ([rows],[cols])."""
+  area_i = len(a[0])*len(a[1])
+  area_j = len(b[0])*len(b[1])
+  area_ij = len(a[0]&b[0])*len(a[1]&b[1])
+  return max(area_ij/area_i, area_ij/area_j)
     
 if __name__ == "__main__":
   main(**dict([s.split('=') for s in sys.argv[1:]]))
